c6313bfbff1bb762de07bde4f862869750301eb9
Marinn <cherurupuriring18@gmail.com>
2025-09-22 09:12:04 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  output: "export", // <-- add this
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
+  output: 'export',                  // enable static export
+  basePath: '/jiya-portfolio',       // repo name
+  assetPrefix: '/jiya-portfolio',    // ensures CSS/JS load correctly
 }
 
 export default nextConfig
